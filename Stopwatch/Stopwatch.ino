3347a4da78208de6747ddb2b2544c901826ae838
morgantegigante <morgannegagne@gmail.com>
2013-04-12 16:07:20 -0300

    Update Stopwatch.ino
    FINAL STOPWATCH

--- Stopwatch/Stopwatch.ino
+++ Stopwatch/Stopwatch.ino
@@ -5,6 +5,8 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 // define some values used by the panel and buttons
 int lcd_key     = 0;
 int adc_key_in  = 0;
+boolean state = false;
+boolean laststate = false;
 #define btnRIGHT  0
 #define btnUP     1
 #define btnDOWN   2
@@ -19,9 +21,6 @@ int adc_key_in  = 0;
 #define V5     700
 #define VNONE  1100
 
-boolean SelectState;
-boolean LastSelectState=false;
-
 // read the buttons
 int read_LCD_buttons()
 {
@@ -33,11 +32,6 @@ int read_LCD_buttons()
  if (adc_key_in < V4)  return btnLEFT;
  if (adc_key_in < V5)  return btnSELECT;
  return btnNONE;  
- if (lcd_key==btnSELECT);
-   {
-   SelectState!=LastSelectState;
-   }
-   delay(200);
 }
 
 int minutes=0;
@@ -50,7 +44,6 @@ void setup()
 }
 
 
-
 void loop()
 {
   lcd.setCursor(0,0);
@@ -59,8 +52,10 @@ void loop()
   lcd.print("seconds:"); 
   lcd.setCursor(9,0);
   lcd.print(minutes);
+
   lcd.setCursor(9,1);
   lcd.print(seconds,DEC);
+  
   lcd_key = read_LCD_buttons(); 
   
 switch (lcd_key)               
@@ -68,12 +63,14 @@ switch (lcd_key)
   case btnRIGHT:
     {
     minutes=minutes+1;
+    delay(200);
     lcd.clear();
     break;
     }
   case btnLEFT:
     {
     minutes=minutes-1;
+    delay(200);
         if (minutes < 0)
         {
           minutes=0;
@@ -84,6 +81,7 @@ switch (lcd_key)
   case btnUP: 
     {
     seconds=seconds+1;
+    delay(200);
         if (seconds > 59)
         {
           seconds=00;
@@ -95,6 +93,7 @@ switch (lcd_key)
   case btnDOWN:
     {
     seconds=seconds-1;
+    delay(200);
     if (seconds < 0)
       {
         seconds=59;
@@ -108,11 +107,10 @@ switch (lcd_key)
     int minStart=minutes;
     int secStart=seconds;
     int timeTotal=(60*minStart+secStart);
-    while (SelectState==true);
-        {
-          {  for (timeTotal; timeTotal>0; timeTotal--);
-               {
-
+    int time = 0;
+    int counter = timeTotal - time;
+             while ((state == false) && (counter > 0))
+             {
                    lcd.setCursor(0,0);
                    lcd.print("minutes:");
                    lcd.setCursor(0,1);
@@ -132,21 +130,51 @@ switch (lcd_key)
                        }
                    if ((minutes ==0) && (seconds == 0)) 
                       {
-                       break; 
+                       delay(2000);
+                       lcd.clear();
+                      for (int i=1; i<5; i++)
+                     { 
+                      lcd.setCursor(0,0);
+                      lcd.print("TIME'S UP BETCH"); 
+                      delay(800);
+                      lcd.clear();
+                      delay(800);
+                     }
+                      break;
                       }
-               }
-
-          }
-        }
-        break;
-    }
+                    lcd_key = read_LCD_buttons();
+                   switch(lcd_key)
+                   {
+                       case btnSELECT:
+                          {
+                            delay(200);
+                            state = !state ;
+                            break;
+                          } 
+                   }
+                   time++;
+                 }
+                 while (state == true)
+                 {
+                  lcd_key = read_LCD_buttons();
+                 switch(lcd_key)
+                   {
+                       case btnSELECT:
+                          {
+                            delay(200);
+                            state = !state ;
+                            break;
+                          } 
+                   }
+                 }
+             break;
+             }
 case btnNONE:
   {
     break;
   }
 }
 }
-
 
 
 // default = 1023
