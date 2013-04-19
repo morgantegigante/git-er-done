18eb37b09e2c2a37c656c01b7886d20e19859570
mjg24@duke.edu <morgannegagne@gmail.com>
2013-04-19 13:21:39 -0400

    Revert "Revert "Revert "Revert "Updates""""
    This reverts commit f51443376bfd998c7a58a13a0686face20053ea0.

--- ControlSystem/Burning.ino
+++ ControlSystem/Burning.ino
@@ -25,6 +25,17 @@ byte flame2[8] = {
 };
 
 byte flame3[8] = {
+  B00000,
+  B00000,
+  B00010,
+  B00011,
+  B00111,
+  B01111,
+  B11110,
+  B01100,
+};
+
+byte flame4[8] = {
   B00010,
   B00010,
   B00111,
@@ -39,21 +50,41 @@ void setup() {
   lcd.createChar(11, flame1);
   lcd.createChar(12, flame2);
   lcd.createChar(13, flame3);
+  lcd.createChar(14, flame4);
   lcd.begin(16, 2);
 }
 
 int burning(){
+  for (int position=0; position<15;position++)
+{
+  lcd.setCursor(position,0);
   lcd.write(byte(11));
-  delay(500);
-  lcd.clear();
-  lcd.write(byte(12));
-  delay(500);
-  lcd.clear();
-  lcd.write(byte(13));
-  delay(500);
-  lcd.clear();
-  delay(500);
 }
+  delay(500);
+  lcd.clear();
+  for (int position=0; position<15;position++)
+{
+  lcd.setCursor(position,0);
+  lcd.write(byte(12));
+}
+  delay(500);
+  lcd.clear();
+  
+    for (int position=0; position<15;position++)
+{
+  lcd.setCursor(position,0);
+  lcd.write(byte(13));
+}
+  delay(500);
+  lcd.clear();
+    for (int position=0; position<15;position++)
+{
+  lcd.setCursor(position,0);
+  lcd.write(byte(14));
+}
+  delay(700);
+}
+
 
 void loop() {
   burning();
