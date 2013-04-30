a79515d03f05b753d74cae6a33844e8246accb91
Kyle Blankenbeckler <kjb23@duke.edu>
2013-04-30 11:59:56 -0400

    Updated Setpoints

--- ControlSystemMaster/HeaterOutput.ino
+++ ControlSystemMaster/HeaterOutput.ino
@@ -1,6 +1,37 @@
-void setSetpoints()
+int checktime()
 {
+  unsigned long timevalue = millis(); 
+  return timevalue;
+}
 
+int setSetpoints()
+{
+  unsigned long currentTime = checktime()/1000;
+  currentTemp = TemperatureReading();
+  int SetSlope = 0;
+  
+  if (currentTime - SysStartTime) < PTIME
+  {
+    stage = 1;
+    SetSlope = (150 - startTemp)/(PTIME-SysStartTime);
+    Setpoint = startTemp + (currentTime*SetSlope);
+  }
+  if (currentTime - SysStartTime) < STIME
+  {
+    stage = 2;
+    Setpoint = 150;
+  }
+  if (currentTime - SysStartTime) < RTIME
+  {
+    stage = 3;
+    SetSlope = (RTEMP-150)/(RTIME-STIME);
+    Setpoint = 150 + (currentTime*SetSlope);
+  }
+  if (currentTime - SysStartTime) < CTIME
+  {
+    stage = 4;
+    Setpoint = 0;
+  }
 }
 
 void HeaterOutput()
@@ -8,7 +39,7 @@ void HeaterOutput()
    int Input = currentTemp;
    myPID.Compute();
    
-    unsigned long now = millis();
+    checktime();
   if(now - windowStartTime>WindowSize)
       { //time to shift the Relay Window
         windowStartTime += WindowSize;
