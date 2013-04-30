594d8ec5a288982802349b0e22ca49e684536063
mjg24@duke.edu <morgannegagne@gmail.com>
2013-04-30 12:12:00 -0400

    "It's errorring in someone else's code..."

--- ControlSystemMaster/HeaterOutput.ino
+++ ControlSystemMaster/HeaterOutput.ino
@@ -10,36 +10,39 @@ int setSetpoints()
   currentTemp = TemperatureReading();
   int SetSlope = 0;
   
-  if (currentTime - SysStartTime) < PTIME
+  if ((currentTime - SysStartTime) < PTIME)
   {
     stage = 1;
     SetSlope = (150 - startTemp)/(PTIME-SysStartTime);
     Setpoint = startTemp + (currentTime*SetSlope);
   }
-  if (currentTime - SysStartTime) < STIME
+  if ((currentTime - SysStartTime) < STIME)
   {
     stage = 2;
     Setpoint = 150;
   }
-  if (currentTime - SysStartTime) < RTIME
+  if ((currentTime - SysStartTime) < RTIME)
   {
     stage = 3;
     SetSlope = (RTEMP-150)/(RTIME-STIME);
     Setpoint = 150 + (currentTime*SetSlope);
   }
-  if (currentTime - SysStartTime) < CTIME
+  if ((currentTime - SysStartTime) < CTIME)
   {
     stage = 4;
     Setpoint = 0;
   }
+  return Setpoint;
 }
 
 void HeaterOutput()
 {  
+   Setpoint = setSetpoints();
    int Input = currentTemp;
+   int now = checktime();
    myPID.Compute();
    
-    checktime();
+    
   if(now - windowStartTime>WindowSize)
       { //time to shift the Relay Window
         windowStartTime += WindowSize;
--- ControlSystemMaster/UserOutput.ino
+++ ControlSystemMaster/UserOutput.ino
@@ -9,9 +9,36 @@ void user_output(){
   
 // display stage of reflow
   lcd.setCursor(0,10);
+  if (stage==1||stage==3){
+    lcd.print("ramp")
+  }
+  else (stage==2){
+    lcd.print("soak")
+  else (stage==4){
+    lcd.print("reflow") 
+  }
+  else (stage==5) {
+    lcd.print("cool")
+  }
   
-  
-  // display time left of entire reflow
-
+ 
+// display time left of entire reflow
+  EndTime=(PTIME+STIME+RTIME+CTIME)*60000+SysStartTime;  // Calculate end time of reflow process in milliseconds
+  TimeNow=millis();                                          // Find time now
+  TimeRemaining=TotalTime-TimeNow;                           // Calculate time remaining in reflow process
+  lcd.setCursor(1, 13);                                  // move cursor to initial position
+  lcd.print(TimeRemaining / 60000);
+  lcd.print(":");
+  //print seconds
+  if (TimeRemaining % (60000) >= 10000) //60000 milliseconds in a minute
+  {
+    lcd.print((time % 60000) / 1000);
+  }
+  else
+  {
+    lcd.print("0"); //print an extra 0 if number is less than 10 seconds
+    lcd.print((time % 60000) / 1000);
+  }
+}
   
 }
