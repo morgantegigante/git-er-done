33d8214496183d4c612c2044b34d033e8d00b889
Kyle Blankenbeckler <kjb23@duke.edu>
2013-05-01 15:53:32 -0400

    did some commenting

--- ControlSystemMaster/HeaterOutput.ino
+++ ControlSystemMaster/HeaterOutput.ino
@@ -1,35 +1,36 @@
-unsigned long setSetpoints()
+unsigned long setSetpoints() //identifying setpoints based on current time and which region of the heating curve it should be in
 {
-  unsigned long currentTime = TimeNow/1000;
+  unsigned long currentTime = TimeNow/1000;              //conversion to seconds
   unsigned long _TimeRemaining = TimeRemaining/1000;
   currentTemp = TemperatureReading();
   unsigned long SetSlope;
   unsigned long _SysStartTime = SysStartTime/1000;
   unsigned long _Setpoint;
   
-  if ((_TimeRemaining) > (STIME+RTIME+CTIME))
+  if ((_TimeRemaining) > (STIME+RTIME+CTIME))            //initial heating region
   {
     stage = 1;
     SetSlope = (150 - startTemp)/(PTIME);
     _Setpoint = startTemp + ((currentTime-_SysStartTime)*SetSlope);
   }
-  else if ((_TimeRemaining) > (STIME+RTIME))
+  else if ((_TimeRemaining) > (STIME+RTIME))            //soak region
   {
     stage = 2;
     SetSlope = 0;
     _Setpoint = 150;
   }
-  else if ((_TimeRemaining) > (STIME))
+  else if ((_TimeRemaining) > (STIME))                  //reflow region
   {
     stage = 3;
     SetSlope = (RTEMP-150)/(STIME); 
     _Setpoint = 150 + ((currentTime-(PTIME+STIME+_SysStartTime))*SetSlope);
   }
-  else if ((_TimeRemaining) > (0))
+  else if ((_TimeRemaining) > (0))                    //cooling region
   {
     stage = 4;
     _Setpoint = 0;
   }
+  //serial debugging
   Serial.print(SetSlope,DEC);
   Serial.println();
   
@@ -40,14 +41,13 @@ void HeaterOutput()
 {  
    int Input = currentTemp;
    int now = TimeNow;
-   myPID.Compute();
-   
+   myPID.Compute();      //computation of PID Output
     
   if(now - windowStartTime>WindowSize)
       { //time to shift the Relay Window
         windowStartTime += WindowSize;
       }
-  if(Output > now - windowStartTime) 
+  if(Output > now - windowStartTime)       //digitalWrite using Output as the control frequency
       {
         digitalWrite(HeaterPin,HIGH);
       }
