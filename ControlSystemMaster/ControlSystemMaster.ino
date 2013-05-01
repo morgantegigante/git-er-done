6260fbe21d258f37a0f44f96c3e6915fdf021828
Kyle Blankenbeckler <kjb23@duke.edu>
2013-05-01 12:38:11 -0400

    idk my bff jill

--- ControlSystemMaster/ControlSystemMaster.ino
+++ ControlSystemMaster/ControlSystemMaster.ino
@@ -27,7 +27,7 @@ int ctime = 60;
 
 unsigned long EndTime;
 unsigned long TimeNow;
-unsigned long TimeRemaining;
+unsigned long TimeRemaining = 0;
 
 // THESE ARE THE OUTPUTS!!! USE THEM! :D
 unsigned long PTIME = 0; //PREHEAT TIME
--- ControlSystemMaster/UserOutput.ino
+++ ControlSystemMaster/UserOutput.ino
@@ -41,28 +41,26 @@ void user_output(){
   
  
 // display time left of entire reflow
-<<<<<<< HEAD
-  unsigned long EndTime=(PTIME+STIME+RTIME+CTIME)*1000+SysStartTime;  // Calculate end time of reflow process in milliseconds
-  unsigned long TimeNow=millis();                                          // Find time now
-  unsigned long TimeRemaining=EndTime-TimeNow;                           // Calculate time remaining in reflow process
-  lcd.setCursor(1, 12);                                  // move cursor to initial position
-=======
+
   EndTime=(PTIME+STIME+RTIME+CTIME)*1000+SysStartTime;  // Calculate end time of reflow process in milliseconds
-  TimeNow=millis();                                          // Find time now
-  TimeRemaining=EndTime-TimeNow;                           // Calculate time remaining in reflow process
-  lcd.setCursor(11, 1);                                  // move cursor to initial position
->>>>>>> 1f9523495bf77bd5132b21e7b7b02235d757fb43
-  lcd.print(TimeRemaining / 60000);
-  lcd.print(":");
-  //print seconds
-  if (TimeRemaining % (60000) >= 10000) //60000 milliseconds in a minute
+  
+  if (TimeRemaining >= 0)
   {
-    lcd.print((TimeRemaining % 60000) / 1000);
-  }
-  else
-  {
-    lcd.print("0"); //print an extra 0 if number is less than 10 seconds
-    lcd.print((TimeRemaining % 60000) / 1000);
+      TimeNow=millis();                                          // Find time now
+      TimeRemaining=EndTime-TimeNow;                           // Calculate time remaining in reflow process
+      lcd.setCursor(11, 1);                                  // move cursor to initial position
+      lcd.print(TimeRemaining / 60000);
+      lcd.print(":");
+      //print seconds
+      if (TimeRemaining % (60000) >= 10000) //60000 milliseconds in a minute
+      {
+        lcd.print((TimeRemaining % 60000) / 1000);
+      }
+      else
+      {
+        lcd.print("0"); //print an extra 0 if number is less than 10 seconds
+        lcd.print((TimeRemaining % 60000) / 1000);
+      }
   }
 }
 
