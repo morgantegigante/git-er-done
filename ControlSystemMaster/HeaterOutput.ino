df08d8cb0a8fe219181d890b387cc9117ec547c8
Kyle Blankenbeckler <kjb23@duke.edu>
2013-05-01 10:23:17 -0400

    Revert "Updated User_Output"
    This reverts commit d71aa7d5ac60a84a1ab685a1fe67e673c586e6f2.

--- ControlSystemMaster/HeaterOutput.ino
+++ ControlSystemMaster/HeaterOutput.ino
@@ -9,30 +9,32 @@ int setSetpoints()
   unsigned long currentTime = checktime()/1000;
   currentTemp = TemperatureReading();
   int SetSlope = 0;
+  SysStartTime = SysStartTime/1000;
+  int _Setpoint = 0;
   
   if ((currentTime - SysStartTime) < PTIME)
   {
     stage = 1;
     SetSlope = (150 - startTemp)/(PTIME-SysStartTime);
-    Setpoint = startTemp + (currentTime*SetSlope);
+    _Setpoint = startTemp + (currentTime*SetSlope);
   }
   if ((currentTime - SysStartTime) < STIME)
   {
     stage = 2;
-    Setpoint = 150;
+    _Setpoint = 150;
   }
   if ((currentTime - SysStartTime) < RTIME)
   {
     stage = 3;
     SetSlope = (RTEMP-150)/(RTIME-STIME);
-    Setpoint = 150 + (currentTime*SetSlope);
+    _Setpoint = 150 + (currentTime*SetSlope);
   }
   if ((currentTime - SysStartTime) < CTIME)
   {
     stage = 4;
-    Setpoint = 0;
+    _Setpoint = 0;
   }
-  return Setpoint;
+  return _Setpoint;
 }
 
 void HeaterOutput()
--- ControlSystemMaster/UserOutput.ino
+++ ControlSystemMaster/UserOutput.ino
@@ -1,37 +1,36 @@
-void user_output(){
+void user_output()
+
+{
 // display desired set temp
   lcd.setCursor(1,0);
-  lcd.print("set:");
-  if (setTemp<=99){
-    lcd.print(" ");}
-  lcd.print(setTemp);
+  lcd.print("set:"+setTemp);
 
 // display current temperature
   lcd.setCursor(0,0);
-  lcd.print("cur:");
-  if (currentTemp<=99){
-  lcd.print(" ");}
-  lcd.print(currentTemp);
+  lcd.print("cur:"+currentTemp);
   
 // display stage of reflow
   lcd.setCursor(0,10);
-  if (stage==1||stage==3){
+  if (stage==1||stage==3)
+  {
     lcd.print("ramp");
   }
-  else if (stage==2){
+  if (stage==2)
+  {
     lcd.print("soak");
   }
-  else if (stage==4){
+  if (stage==4)
+  {
     lcd.print("reflow");
   }
-  else if (stage==5) {
+  if (stage==5)
+  {
     lcd.print("cool");
   }
-
   
  
 // display time left of entire reflow
-  unsigned long EndTime=(PTIME+STIME+RTIME+CTIME)*60000+SysStartTime;  // Calculate end time of reflow process in milliseconds
+  int EndTime=(PTIME+STIME+RTIME+CTIME)*1000+SysStartTime;  // Calculate end time of reflow process in milliseconds
   unsigned long TimeNow=millis();                                          // Find time now
   unsigned long TimeRemaining=EndTime-TimeNow;                           // Calculate time remaining in reflow process
   lcd.setCursor(1, 13);                                  // move cursor to initial position
